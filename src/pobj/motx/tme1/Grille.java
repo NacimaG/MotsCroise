85a639079b8a817b7d9258b89d3028b7df6a0bdb
3704383 <3704383@ppti-14-303-06.ufr-info-p6.jussieu.fr>
2019-02-04 13:16:40 +0100

    Création classe Grille

--- src/pobj/motx/tme1/Grille.java
+++ src/pobj/motx/tme1/Grille.java
@@ -4,6 +4,13 @@ public class Grille {
 	private int hauteur;
 	private int largeur;
 	private Case[][] grille;
+	/**
+	 * 
+	 * @param hauteur  de la grille
+	 * @param largeur de la grille
+	 * 
+	 * initialisation de la liste des cases contenues dans la grille à des cases vides (' ')
+	 */
 	
 	public Grille(int hauteur, int largeur){
 		this.hauteur = hauteur;
@@ -15,9 +22,18 @@ public class Grille {
 			}
 		}
 	}
+	/**
+	 * 
+	 * @param lig 
+	 * @param col
+	 * @return retourner une case dans la grille de numéroro de ligne "lig" et numéro de colonne "col" 
+	 */
 	public Case getCase(int lig, int col){
 		return grille[lig][col];
 	}
+	/**
+	 * affichage de la grille
+	 */
 	public String toString(){
 		StringBuilder  sb= new StringBuilder();
 		for(int i=0;i<hauteur;i++){
@@ -32,12 +48,24 @@ public class Grille {
 		
 		return sb.toString();
 	}
+	/**
+	 * 
+	 * @return nombre de ligne de la grille
+	 */
 	public int nbLig(){
 		return this.hauteur;
 	}
+	/**
+	 * 
+	 * @return nombre de colonne de la grille
+	 */
 	public int nbCol(){
 		return this.largeur;
 	}
+	/**
+	 * 
+	 * @return une copie d ela grille (avec des copies de cases (les deuxx pointent des cases différentes masi avec des mêmes valeurs)
+	 */
 	public Grille copy(){
 		Grille g;
 		g = new Grille(this.hauteur, this.largeur);
